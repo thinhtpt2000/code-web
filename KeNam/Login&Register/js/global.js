862c0a6e8e678c84995f3eee5f398bbb93f18344
kenam1999 <kenam1999@users.noreply.github.com>
2019-07-15 08:45:10 +0700

    login&rigister v1.0.2

---
+++ KeNam/Login&Register/js/global.js
@@ -0,0 +1,25 @@
+function validateLoginForm() {
+    var name = document.loginForm.username.value;
+    var password = document.loginForm.password.value;
+
+    if (name == null || name == "") {
+        alert("Username can't be blank");
+        return false;
+    } else if (password.length < 6) {
+        alert("Password must be at least 6 characters long.");
+        return false;
+    }
+}
+
+function validateSigninForm() {
+    var name = document.getElementById("username");
+    var password = document.signForm.password.value;
+
+    if (name == null || name == "") {
+        alert("Username can't be blank");
+        return false;
+    } else if (password.length < 6) {
+        alert("Password must be at least 6 characters long.");
+        return false;
+    }
+}  
